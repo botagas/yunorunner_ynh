9c1f49a0cfec24c0279d1ecc5351476c0e89e9de
Patrick <49989644+botagas@users.noreply.github.com>
2025-01-10 20:12:12 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,7 +17,7 @@ _git_clone_or_pull() {
     repo_url="${2:-}"
 
     if [[ -z "$repo_url" ]]; then
-        repo_url=$(ynh_read_manifest --manifest_key="code")
+        repo_url=$yunorunner_repository
     fi
 
     if [ -d "$repo_dir" ]; then
